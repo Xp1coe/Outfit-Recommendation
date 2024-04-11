aac832d0b88b66afb275cb67ced81d7bad78838c
Harsh Shah <202101622@vupune.ac.in>
2024-04-12 00:12:55 +0530

    Ui

--- outfit-recommend/src/Components/Values.jsx
+++ outfit-recommend/src/Components/Values.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 const Values = () => {
     return (
-        <div className='text-xl font-mono text-center border border-black w-[80%] m-40 p-4'>
+        <div className='text-xl font-mono text-center border border-black w-[80%] m-40'>
             <form>
-
-                <h2 className='font-serif font-bold '>Suggest me about the Outfit</h2>
-                <hr className='border border-black' />
+                <h2 className='font-serif font-bold p-2 text-2xl bg-[#33302d] text-white'>Suggest me about the Outfit</h2>
                 <div className=''>
-                    <ul className='flex gap-16 px-[200px] m-10'>
+                    <ul className='flex gap-16 px-[200px] m-10 p-4'>
                         <li>
                             <label htmlFor="">Gender:</label>
                             <select id="dropdown" className='border border-black p-2'>
@@ -30,13 +28,13 @@ const Values = () => {
                             <label htmlFor="">Weather</label>
                             <select id="dropdown" className='border border-black p-2'>
                                 <option value="default">Select...</option>
-                                <option value="option1">Male</option>
-                                <option value="option2">Female</option>
+                                <option value="option1">Summmer</option>
+                                <option value="option2">Winter</option>
                             </select>
                         </li>
                     </ul>
                 </div>
-                <button type='submit' className='border border-black p-2 w-[100px] hover:bg-black hover:text-white ease-in-out duration-500'>Submit</button>
+                <button type='submit' className='border border-black p-2 my-4 w-[100px] hover:bg-black hover:text-white ease-in-out duration-500'>Submit</button>
             </form>
         </div>
     )
